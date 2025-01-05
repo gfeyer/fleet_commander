cd35c6023d5f25945b2a20a494a3ddc05da61bb8
gfeyer <feyervlad@gmail.com>
2025-01-05 15:48:41 -0500

    move drone system to entt views

--- src/Systems/DroneTransferSystem.hpp
+++ src/Systems/DroneTransferSystem.hpp
@@ -1,34 +1,29 @@
 #ifndef DRONE_TRANSFER_SYSTEM_HPP
 #define DRONE_TRANSFER_SYSTEM_HPP    
 
+#include <execution>
+
 #include "Game/GameEntityManager.hpp"
 
 #include "Components/DroneTransferComponent.hpp"
 
 namespace Systems {
 
+
     void DroneTransferSystem(Game::GameEntityManager& manager, float dt) {
 
-        auto entities = manager.getAllEntityIDs();
-        for (auto id : entities) {
+        for(auto&& [id, transfer, faction, garisson] : manager.view<
+            Components::DroneTransferComponent,
+            Components::FactionComponent,
+            Components::GarissonComponent>().each()) {
+                
+            if(faction.faction != transfer.faction){
+                // if the factions changed, remove the order
+                manager.removeComponent<Components::DroneTransferComponent>(id);
+            }
 
-            auto* droneTransferComp = manager.getComponent<Components::DroneTransferComponent>(id);
-
-            if (droneTransferComp) {
-                // Check if faction is the same
-                auto* factionComp = manager.getComponent<Components::FactionComponent>(id);
-                if(factionComp && factionComp->faction != droneTransferComp->faction){
-                    manager.removeComponent<Components::DroneTransferComponent>(id);
-                    continue;
-                }
-
-                // Check if there's any drones to transfer
-                auto* garissonComp = manager.getComponent<Components::GarissonComponent>(id);
-                if(garissonComp && garissonComp->getDroneCount() > 0){
-                    auto source = droneTransferComp->source;
-                    auto target = droneTransferComp->target;
-                    manager.addOrReplaceComponent<Components::AttackOrderComponent>(source, source, target);
-                }
+            if(garisson.getDroneCount() > 0){
+                manager.addOrReplaceComponent<Components::AttackOrderComponent>(transfer.source, transfer.source, transfer.target);
             }
         }
     }
